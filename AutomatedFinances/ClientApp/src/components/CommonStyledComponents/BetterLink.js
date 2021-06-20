6a3a3f8e1d3ce42b733c9b330f44fefbf4bfe5a3
Albert Ferguson <albertferguson118@gmail.com>
2021-06-20 15:59:56 +1000

    fixed transient props issue in betterlinks

--- AutomatedFinances/ClientApp/src/components/CommonStyledComponents/BetterLink.js
+++ AutomatedFinances/ClientApp/src/components/CommonStyledComponents/BetterLink.js
@@ -5,38 +5,45 @@ import { Link } from 'react-router-dom';
 import "../../styles/homestyles.css";
 
 const StyledHyperLink = styled(Link)`
-        color: ${props => props.color ? props.color : "inherit"};
-        width: ${props => props.width ? props.width : "inherit"};
-        font-weight: ${props => props.fontWeight ? props.fontWeight : "400"};
-        line-height: ${props => props.lineHeight ? props.lineHeight : "1rem"};
-        letter-spacing: ${props => props.letterSpacing ? props.letterSpacing : "0.02rem"};
+        color: ${props => props.$color ? props.$color : "inherit"};
+        width: ${props => props.$width ? props.$width : "inherit"};
+        font-weight: ${props => props.$fontWeight ? props.$fontWeight : "400"};
+        line-height: ${props => props.$lineHeight ? props.$lineHeight : "1rem"};
+        letter-spacing: ${props => props.$letterSpacing ? props.$letterSpacing : "0.02rem"};
         place-content: center;
         place-items: center;
         padding: 0 var(--navigation-gap);
         text-decoration: none !important;
-        font-size: ${props => props.fontSize ? props.fontSize : "18px"};
+        font-size: ${props => props.$fontSize ? props.$fontSize : "18px"};
         
         &:hover, &:focus, &:visited, &:link, &:active {
             text-decoration: none !important;
         }
         
         &:hover {
-            color: ${props => props.hoverColor ? props.hoverColor : "var(--gold-metallic)"};
+            color: ${props => props.$hoverColor ? props.$hoverColor : "var(--gold-metallic)"};
         }
         
     `;
 
+
 export default function BetterLink(props) {
+/* 
+ *  use $ prefix to mark a prop as transient, avoids it being passed to the
+ *  underlying component.
+ *  https://stackoverflow.com/questions/49834251/how-to-extend-styled-component-without-passing-props-to-underlying-dom-element 
+ */
+
     return (
         <StyledHyperLink
             className="better-link"
-            color={props.color}
-            width={props.width}
-            fontSize={props.fontSize}
-            hoverColor={props.hoverColor}
-            fontWeight={props.fontWeight}
-            lineHeight={props.lineHeight}
-            letterSpacing={props.letterSpacing}
+            $color={props.color}
+            $width={props.width}
+            $fontSize={props.fontSize}
+            $hoverColor={props.hoverColor}
+            $fontWeight={props.fontWeight}
+            $lineHeight={props.lineHeight}
+            $letterSpacing={props.letterSpacing}
             to={{ pathname: props.Link }}
         >
             { props.LinkText}
