0dd121821d1934b0ab0fdde94ae50555652d81b6
theQuantumMechanic <albertferguson118@gmail.com>
2021-10-04 00:25:13 +1100

    employment lib hangover stuff

---
+++ EmploymentLibrary/LoginCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EmploymentLibrary
+{
+    public class LoginCredentials
+    {
+        public static List<string> UTSLoginCredentials()
+        {
+            var vals = new List<string>() { "13611165", "ADC457dede:A", "" };
+            return vals;
+        }
+    }
+}
