9ca63112afb11ddd29fcdf1aa7196c739ad14b5e
theQuantumMechanic <albertferguson118@gmail.com>
2022-05-27 19:10:31 +1000

    Remove old code

--- AutomatedFinances.Migrations/Program.cs
+++ AutomatedFinances.Migrations/Program.cs
@@ -1,48 +1,12 @@
-﻿using FluentMigrator.Runner;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-
-namespace AutomatedFinances.Migrations
+﻿namespace AutomatedFinances.Migrations
 {
     public static class Program
     {
-        const bool RollbackLastChange = false;
-        
-        private const string DbConn = "Server=192.168.1.134;Database=AutomatedFinancesDb;User Id=sa;Password=p9kNCTHi@91a;";
-        
-        private static void Main() {
-            var serviceProvider = BuildServiceProvider(DbConn);
+        private const string IridiumDbAuth =
+            "Server=192.168.1.134;Database=AutomatedFinancesDb;User Id=sa;Password=p9kNCTHi@91a;";
 
-            using var scope = serviceProvider.CreateScope();
-            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
-
-            // TODO: pipe a console arg
-            // ReSharper disable once ConditionIsAlwaysTrueOrFalse'
-            // ReSharper disable once HeuristicUnreachableCode
-            if (RollbackLastChange) {
-                RollBackLastMigration(runner);
-            }
+        public static void Main() {
             
-            UpdateDatabase(runner);
         }
-
-        private static IServiceProvider BuildServiceProvider(string dbConn) {
-            return new ServiceCollection()
-                .AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                    .AddSqlServer()
-                    .WithGlobalConnectionString(dbConn)
-                    // Define the assembly containing the migrations
-                    // TODO: better way to register migrations than this manual shit
-                    .ScanIn(typeof(AddFinanceDomainTables).Assembly).For.Migrations()
-                    .ScanIn(typeof(InsertPaymentMethods).Assembly).For.Migrations()
-                )
-                .AddLogging(lb => lb.AddFluentMigratorConsole())
-                .BuildServiceProvider(false);
-        }
-
-        private static void UpdateDatabase(IMigrationRunner runner) => runner.MigrateUp();
-
-        private static void RollBackLastMigration(IMigrationRunner runner) => runner.Rollback(1);
     }
 }
