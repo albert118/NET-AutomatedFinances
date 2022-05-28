670f5e90483a8b86dad6e5ab3c74ed457cc8f1b8
theQuantumMechanic <albertferguson118@gmail.com>
2022-05-28 15:36:36 +1000

    Simplify Program.cs (drops comments)

--- AutomatedFinances.Infrastructure/Program.cs
+++ AutomatedFinances.Infrastructure/Program.cs
@@ -1,27 +1,12 @@
-﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading.Tasks;
-
-namespace AutomatedFinances.Infrastructure
+﻿namespace AutomatedFinances.Infrastructure
 {
     internal class Program
     {
-        // const string ServiceName = "automatedfinances";
-        
-        // private static readonly ServerSettings ServerSettings = ConfigureServerSettings();
-
         private static async Task Main(string[] args) {
-            
-
-            // start a new container with the server settings configuration.
             Console.WriteLine("AutoFac Setup running...");
-            // var container = SetupAutofacContainer(ServerSettings);
-            // container.BeginLifetimeScope();
-            
+
             var host = CreateConsoleHost(args);
-            
+
             Console.WriteLine("Starting the console app...");
             await host.RunConsoleAsync();
 
@@ -30,8 +15,7 @@ namespace AutomatedFinances.Infrastructure
             // container.Dispose();
         }
 
-        private static IHostBuilder CreateConsoleHost(string[] args) 
-        {
+        private static IHostBuilder CreateConsoleHost(string[] args) {
             var hostBuilder = Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
 #if DEBUG
@@ -41,44 +25,5 @@ namespace AutomatedFinances.Infrastructure
 #endif
             return hostBuilder;
         }
-
-        // private static IContainer SetupAutofacContainer(ServerSettings serverSettings)
-        // {
-        //     var builder = new ContainerBuilder();
-        //
-        //     builder
-        //         .RegisterInstance(serverSettings)
-        //         .AsSelf()
-        //         .SingleInstance();
-        //
-        //     RegisterAndConfigureDatabase(builder, serverSettings);
-        //
-        //     return builder.Build();
-        // }
-
-        // private static void RegisterAndConfigureDatabase(
-        //     ContainerBuilder containerBuilder, 
-        //     ServerSettings serverSettings)
-        // {
-        //     containerBuilder.AddEntityFramework();
-            //
-            // containerBuilder
-            //     .RegisterInstance(serverSettings.ToDataBaseSettings())
-            //     .AsSelf()
-            //     .SingleInstance();
-            //
-            // containerBuilder
-            //     .RegisterType<AutomedFinancesDbContext>()
-            //     .As<IAutomedFinancesDbContext>()
-            //     .InstancePerLifetimeScope();
-        // }
-        //
-        // private static ServerSettings ConfigureServerSettings() =>
-        //     new() {
-        //         IridiumServerPath = "192.168.133",
-        //         AutomatedFinancesDbName = "AutomatedFinancesDb",
-        //         AutomatedFinancesDbPassword = "p9kNCTHi@91a",
-        //         AutomatedFinancesDbUser = "sa"
-        //     };
     }
 }
