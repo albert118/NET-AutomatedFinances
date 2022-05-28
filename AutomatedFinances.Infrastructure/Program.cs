c5916122b9c3815d6c8eadef7338489294e7de73
theQuantumMechanic <albertferguson118@gmail.com>
2022-05-28 15:30:09 +1000

    Drop old content from infrastructure

--- AutomatedFinances.Infrastructure/Program.cs
+++ AutomatedFinances.Infrastructure/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using AutomatedFinances.Application.Interfaces;
-using AutomatedFinances.Infrastructure.Data;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading.Tasks;
@@ -10,17 +8,17 @@ namespace AutomatedFinances.Infrastructure
 {
     internal class Program
     {
-        const string ServiceName = "automatedfinances";
+        // const string ServiceName = "automatedfinances";
         
-        private static readonly ServerSettings ServerSettings = ConfigureServerSettings();
+        // private static readonly ServerSettings ServerSettings = ConfigureServerSettings();
 
         private static async Task Main(string[] args) {
             
 
             // start a new container with the server settings configuration.
             Console.WriteLine("AutoFac Setup running...");
-            var container = SetupAutofacContainer(ServerSettings);
-            container.BeginLifetimeScope();
+            // var container = SetupAutofacContainer(ServerSettings);
+            // container.BeginLifetimeScope();
             
             var host = CreateConsoleHost(args);
             
@@ -29,7 +27,7 @@ namespace AutomatedFinances.Infrastructure
 
             // end of lifetime
             Console.WriteLine("Shutting down the console app...");
-            container.Dispose();
+            // container.Dispose();
         }
 
         private static IHostBuilder CreateConsoleHost(string[] args) 
@@ -44,43 +42,43 @@ namespace AutomatedFinances.Infrastructure
             return hostBuilder;
         }
 
-        private static IContainer SetupAutofacContainer(ServerSettings serverSettings)
-        {
-            var builder = new ContainerBuilder();
+        // private static IContainer SetupAutofacContainer(ServerSettings serverSettings)
+        // {
+        //     var builder = new ContainerBuilder();
+        //
+        //     builder
+        //         .RegisterInstance(serverSettings)
+        //         .AsSelf()
+        //         .SingleInstance();
+        //
+        //     RegisterAndConfigureDatabase(builder, serverSettings);
+        //
+        //     return builder.Build();
+        // }
 
-            builder
-                .RegisterInstance(serverSettings)
-                .AsSelf()
-                .SingleInstance();
-
-            RegisterAndConfigureDatabase(builder, serverSettings);
-
-            return builder.Build();
-        }
-
-        private static void RegisterAndConfigureDatabase(
-            ContainerBuilder containerBuilder, 
-            ServerSettings serverSettings)
-        {
-            containerBuilder.AddEntityFramework();
-
-            containerBuilder
-                .RegisterInstance(serverSettings.ToDataBaseSettings())
-                .AsSelf()
-                .SingleInstance();
-            
-            containerBuilder
-                .RegisterType<AutomedFinancesDbContext>()
-                .As<IAutomedFinancesDbContext>()
-                .InstancePerLifetimeScope();
-        }
-
-        private static ServerSettings ConfigureServerSettings() =>
-            new() {
-                IridiumServerPath = "192.168.133",
-                AutomatedFinancesDbName = "AutomatedFinancesDb",
-                AutomatedFinancesDbPassword = "p9kNCTHi@91a",
-                AutomatedFinancesDbUser = "sa"
-            };
+        // private static void RegisterAndConfigureDatabase(
+        //     ContainerBuilder containerBuilder, 
+        //     ServerSettings serverSettings)
+        // {
+        //     containerBuilder.AddEntityFramework();
+            //
+            // containerBuilder
+            //     .RegisterInstance(serverSettings.ToDataBaseSettings())
+            //     .AsSelf()
+            //     .SingleInstance();
+            //
+            // containerBuilder
+            //     .RegisterType<AutomedFinancesDbContext>()
+            //     .As<IAutomedFinancesDbContext>()
+            //     .InstancePerLifetimeScope();
+        // }
+        //
+        // private static ServerSettings ConfigureServerSettings() =>
+        //     new() {
+        //         IridiumServerPath = "192.168.133",
+        //         AutomatedFinancesDbName = "AutomatedFinancesDb",
+        //         AutomatedFinancesDbPassword = "p9kNCTHi@91a",
+        //         AutomatedFinancesDbUser = "sa"
+        //     };
     }
 }
