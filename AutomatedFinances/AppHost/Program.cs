d660f8227198b840dc792d9bd30dcdb836045b5a
theQuantumMechanic <albertferguson118@gmail.com>
2022-06-27 19:09:37 +1000

    AppHost added with Swagger and Dummy Controller

---
+++ AutomatedFinances/AppHost/Program.cs
@@ -0,0 +1,33 @@
+namespace AppHost;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var builder = WebApplication.CreateBuilder();
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
