7810bf626d798b89220194f5dfa6df2d7e4cd4d8
theQuantumMechanic <albertferguson118@gmail.com>
2022-08-12 21:29:56 +1000

    Added the initial app files

--- frontend/icarus/src/App.js
+++ frontend/icarus/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
+import Layout from './components/Layout';
 import './App.css';
+import './dove-theme/styles.css';
 
 function App() {
   return (
     <div className="App">
+      <Layout />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        This is watching the files!
+
       </header>
     </div>
   );
