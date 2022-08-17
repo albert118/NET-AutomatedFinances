085bae6f89be8aea590c9eeff80c26edfa193ecf
theQuantumMechanic <albertferguson118@gmail.com>
2022-08-17 22:51:16 +1000

    Transaction view incl. header and form components

--- frontend/icarusUI/src/pages/TransactionRecords/TransactionRecordsView.js
+++ frontend/icarusUI/src/pages/TransactionRecords/TransactionRecordsView.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import AddTransactionRecord from './components/Forms/AddTransactionRecord';
+import PageHeader from './components/PageHeader';
 
 export default function TransactionRecordsView() {
     return (
         <div className="p-transaction-records-view">
-            Transaction Records page
+            <PageHeader />
+            <AddTransactionRecord />
         </div>
     );
 };
