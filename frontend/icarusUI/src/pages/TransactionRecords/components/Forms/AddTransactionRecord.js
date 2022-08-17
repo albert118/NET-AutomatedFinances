9e7e736e58cff67e370f7cdd349d741c96bae1d3
theQuantumMechanic <albertferguson118@gmail.com>
2022-08-17 23:50:31 +1000

    Include new fields and demo text replaced

--- frontend/icarusUI/src/pages/TransactionRecords/components/Forms/AddTransactionRecord.js
+++ frontend/icarusUI/src/pages/TransactionRecords/components/Forms/AddTransactionRecord.js
@@ -2,6 +2,8 @@ import CommonButton from 'components/CommonButton';
 import React from 'react';
 import FormTitle from './FormTitle';
 import TextField from './TextField';
+import DateField from './DateField';
+import CurrencyField from './CurrencyField';
 
 export default function AddTransactionRecord() {
     return (
@@ -9,11 +11,12 @@ export default function AddTransactionRecord() {
             <div className="form-content-grid--contained">
                 <FormTitle title="Transaction Records" />
                 <div className="form-field-grid">
-                    <TextField className="left-field" label="First name" prompt="Albert"/>
-                    <TextField className="right-field" label="Last name" prompt="Ferguson" />
-                    <TextField className="full-width" label="Email" prompt="albertferguson@test.com" />
-                    <TextField className="full-width" label="Password" prompt="**************" />
-                    <br />
+                    <TextField className="left-field" label="Transaction name" prompt="An easy to identify name"/>
+                    <DateField className="right-field" label="Date"/>
+                    <TextField className="full-width" label="Description" prompt="A brief description of the transaction" />
+                    <CurrencyField className="left-field" label="Total cost" prompt="$" />
+                    <TextField className="right-field" label="(External) transaction reference" prompt="The receipt number typically" />
+                    
                     <CommonButton className="left-field" buttonText="Cancel"/>
                     <CommonButton className="right-field" buttonText="Submit"/>
                 </div>
