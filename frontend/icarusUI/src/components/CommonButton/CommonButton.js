a30167fdadce973f885e0c814f551408d9881259
theQuantumMechanic <albertferguson118@gmail.com>
2022-10-10 01:00:04 +1100

    updated common button to support nav linking

--- frontend/icarusUI/src/components/CommonButton/CommonButton.js
+++ frontend/icarusUI/src/components/CommonButton/CommonButton.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const CommonButton = props => {
-    const { className, buttonText } = props;
+    const { className, buttonText, link } = props;
+
+    let push  = useNavigate();
+
+    const handleClick = () => {
+        if (link) push(link);
+    };
 
     return (
-        <button type="button" className={`common-btn ${className}`}>
+        <button 
+            type="button" 
+            className={`common-btn ${className}`}
+            onClick={handleClick}
+        >
             { buttonText }
         </button>
     );
