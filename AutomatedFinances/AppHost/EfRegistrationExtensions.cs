e6c7f193ebdfe81c3d0a040f06b2450ece65099a
theQuantumMechanic <albertferguson118@gmail.com>
2022-10-10 00:54:35 +1100

    Extended db settings and updated launch to handle maria db (also added new controller + DI)

--- AutomatedFinances/AppHost/EfRegistrationExtensions.cs
+++ AutomatedFinances/AppHost/EfRegistrationExtensions.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using AutomatedFinances.Application.Interfaces;
 using AutomatedFinances.Infrastructure.Data.TradingTransactionsDb;
+using AutomatedFinances.Infrastructure.Data.TransactionRecordsDb;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppHost;
@@ -26,6 +27,25 @@ internal static class EfRegistrationExtensions
         return containerBuilder;
     }
 
+    public static ContainerBuilder AddTransactionContext(this ContainerBuilder containerBuilder)
+    {
+        containerBuilder
+            .AddDbContextOptions<TransactionReadDbContext>()
+            .AddDbContextOptions<TransactionWriteDbContext>();
+
+        containerBuilder
+            .RegisterType<TransactionReadDbContext>()
+            .As<ITransactionReadDbContext>()
+            .InstancePerLifetimeScope();
+
+        containerBuilder
+            .RegisterType<TransactionWriteDbContext>()
+            .As<ITransactionWriteDbContext>()
+            .InstancePerLifetimeScope();
+
+        return containerBuilder;
+    }
+
     private static ContainerBuilder AddDbContextOptions<TContext>(this ContainerBuilder containerBuilder)
         where TContext : DbContext
     {
@@ -35,7 +55,7 @@ internal static class EfRegistrationExtensions
                 var dbSettings = sp.Resolve<DatabaseSettings>();
                 return new DbContextOptionsBuilder<TContext>()
                     .UseLoggerFactory(loggerFactory)
-                    .UseSqlServer(dbSettings.ConnectionString)
+                    .UseMySql(dbSettings.ConnectionString, dbSettings.ServerVersion)
                     .EnableDetailedErrors()
                     .EnableSensitiveDataLogging()
                     .Options; // make sure to return options here! Otherwise we'll register the builder
