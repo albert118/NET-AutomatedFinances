dd1464a9ab19bd2ba370d0ae09a1086df0ffe870
theQuantumMechanic <albertferguson118@gmail.com>
2022-10-10 01:40:30 +1100

    Dropped alpha prototype of this repo's namesake (#30)
    * Dropped alpha prototype of this repo's namesake
    
    * Removed the original docs, commonLibrary, and EmploymentLibrary libs/projects (also dropped some old request templates)
    
    * Dropped the remainder of the original project python code

--- AutomatedFinances/test/__init__.py
+++
@@ -1,8 +0,0 @@
-# needed for Sphinx autodoc feature.
-
-"""
-.. module:: tests
-    :platform: Unix, Windows
-    :synopsis: The testing module.
-.. moduleauthor:: Albert Ferguson <albertferguson118@gmail.com>
-"""
